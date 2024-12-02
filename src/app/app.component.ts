aa314fb169863ad5dd89e753918b67ebe2890279
marics14 <162575459+marics14@users.noreply.github.com>
2024-12-02 08:53:31 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent], // Certifique-se de importar o componente Home
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'] // Corrija para "styleUrls" (plural)
+})
+export class AppComponent {
+  title = 'mariana-angular';
+}
